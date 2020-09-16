ce670df6d6925aa026c3a05fc342565191bc2717
soon <soon@soons-MacBook-Air.local>
2020-09-16 11:44:14 +0900

    Adding Head component and link - lec 76

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Route, Switch, Link } from 'react-router-dom';
 import HomePage from './pages/homepages/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
+
 import './App.css';
 
 const HatsPage = (props) => {
@@ -28,6 +30,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route exact path="/shop" component={ShopPage} />

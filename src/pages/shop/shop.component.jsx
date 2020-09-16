8ef24886f166b68f0b8d19bfaba7a076209fd966
soon <soon@soons-MacBook-Air.local>
2020-09-16 09:56:04 +0900

    updating our app with a shop page, collection preveiw componenet - lec 75

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -6,14 +6,16 @@ import CollectionPreview from '../../components/collection-preview/collection-pr
 class ShopPage extends React.Component {
     constructor(props){
         super(props);
-
         this.state = {
             collections: SHOP_DATA
         }
 
     }
+
     render(){
         const { collections } = this.state;
+        console.log(this.state) // collections: (5) [{…}, {…}, {…}, {…}, {…}]
+        console.log(collections) // [{…}, {…}, {…}, {…}, {…}]
         return (
             <div className='shop-page'>
                 {

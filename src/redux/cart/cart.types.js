015859ddcd0231ad0e7bf7c821871b9bdbb7202b
soon <soon@soons-MacBook-Air.local>
2020-10-04 10:29:57 +0900

    added addItem and removeItem functionality to checkout item component

--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -1,6 +1,8 @@
 const CartActionTypes = {
     TOGGLE_CART_HIDDEN: 'TOGGLE_CART_HIDDEN',
-    ADD_ITEM: 'ADD_ITEM'
+    ADD_ITEM: 'ADD_ITEM',
+    REMOVE_ITEM: 'REMOVE_ITEM',
+    CREAR_ITEM_FROM_CART: 'CREAR_ITEM_FROM_CART',
   };
   
   export default CartActionTypes;
